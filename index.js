5b788f294dc00f89446016da03fe4e24652f3849
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-05-11 18:21:43 +0545

    basic setup

---
+++ index.js
@@ -0,0 +1,17 @@
+let sub_btn = document.getElementById("sub")
+let unsub_btn = document.getElementById("unsub")
+
+
+const subscribe = () => {
+   // todo:
+   // 1. Check if the user already exist
+   // 2. If not add to the db
+}
+const unsubscribe = () => {
+    // todo:
+    // 1. check if the user exist in db
+    // 2. if yes unsubscibe from the list
+}
+
+sub_btn.addEventListener("click", subscribe);
+unsub_btn.addEventListener("click", unsubscribe);
